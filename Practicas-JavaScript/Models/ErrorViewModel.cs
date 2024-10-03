de77f100fa6f390ebfafbe0aae497e9a003882d6
IlanTauscher <48240500@est.ort.edu.ar>
2024-10-03 20:12:12 -0300

    Act1 lista

---
+++ Practicas-JavaScript/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Practicas_JavaScript.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
