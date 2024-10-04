8ad3f7ca4ec8ef74c654409347f4fa662496a51c
IlanTauscher <48240500@est.ort.edu.ar>
2024-10-03 22:32:29 -0300

    listo el 2

--- Practicas-JavaScript/obj/Debug/net8.0/Practicas-JavaScript.AssemblyInfo.cs
+++ Practicas-JavaScript/obj/Debug/net8.0/Practicas-JavaScript.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practicas-JavaScript")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de77f100fa6f390ebfafbe0aae497e9a003882d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practicas-JavaScript")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practicas-JavaScript")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
